796b0cb8f3076f5efcbd33544447b60c9f95c954
mkgktn <mkgktn@gmail.com>
2020-08-02 18:42:41 -0500

    updating Hello program file...

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
 //	cmd/ go to Jenkins folder
 //	java -jar jenkins.war --httpPort=9191   // for specific port //othewise it 
 //						//will start with //default 8080 port
-
+//
 public class Hello{
     public static void main(String [] args){
 	for (int i=1; i<=10; i++) {
